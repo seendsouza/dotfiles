680794e960ff4ed66e40a845e7bc8d37bdcb5205
Sean D'Souza <sdammobubbles@gmail.com>
2020-10-29 22:35:05 -0400

    Make separate monitors easy

--- bin/bin/separate_monitor.sh
+++ bin/bin/separate_monitor.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+intern=eDP1
+extern=DP1
+
+if xrandr | grep "$extern disconnected"; then
+    xrandr --output "$extern" --off --output "$intern" --auto
+else
+    xrandr --output "$intern" --off --output "$extern" --auto
+fi
 ~/.config/i3/polybar.sh
 ~/.config/i3/feh.sh
